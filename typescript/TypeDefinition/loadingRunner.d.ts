b9cc02f6cbaa0a524d79d22bcb422647a86b3d56
j6k1 <blank>
2014-07-10 14:38:15 +0900

    typescript用の型定義ファイルを追加

---
+++ typescript/TypeDefinition/loadingRunner.d.ts
@@ -0,0 +1,11 @@
+﻿declare var LoadingRunner: LoadingRunnerStatic;
+
+interface LoadingRunnerStatic {
+    new (initializer: () => (degree: number) => void, rotation_time?: number);
+    new (initializer: () => void, rotation_time?: number);
+    canvasLoaderCreator(canvasId: string, color?: { r: number; g: number; b: number }): () => (degree: number) => void;
+}
+interface LoadingRunner {
+    start(): void;
+    stop(): void;
+}
